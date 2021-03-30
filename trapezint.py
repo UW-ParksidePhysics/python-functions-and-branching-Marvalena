0bb51540e96281cc9e9700b87193f508bdcf3338
Marvalena <marvelousmarvalena@gmail.com>
2021-03-30 18:28:15 +0000

    aha tings have been done

---
+++ trapezint.py
@@ -0,0 +1,14 @@
+#print ('Part a:')
+#def F(f,a,b):
+#  trapezint1=((b-a)/2)*(f(a)-f(b))
+#  return trapezint1
+
+#print ('Part b:')
+#from numpy import pi, cos, sin
+#print (F(cos,0,pi))
+#print (F(sin,0,pi))
+#print (F(sin,0,(pi/2)))
+
+#print ('Part c:')
+
+#decided not to do
